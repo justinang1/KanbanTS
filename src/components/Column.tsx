1fbee2179fa9609bcd8f8045b9b75583ec4a53b3
Justin Ang <justinang13@gmail.com>
2020-03-11 14:10:49 -0700

    Finished formatting of tasks. Added prompt on 'create task' button. Started boilerplate redux

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -12,18 +12,20 @@ type ColProps = {
 
 function Column({name, id} : ColProps) {
   let headerColor: string = "";
-  let childElem: Array<Object> = [];
+  let childElem: Array<Object> = [<Task text='+ Click to Add Task' id={id} type={-1}/>];
   switch(id){
     case 0:
-      headerColor = "header-red";
-      childElem.push(<Task text="test text 1" id={0}/>)
+      headerColor = 'header-red';
+      childElem.unshift(<Task text='Implement Task Creation' id={id} type={0}/>)
+      childElem.unshift(<Task text='Implement Redux Store' id={id} type={0}/>)
       break;
     case 1:
-      headerColor = "header-orange";
-      childElem.push(<Task text="test text 2" id={0}/>)
+      headerColor = 'header-orange';
+      childElem.unshift(<Task text='test text 2' id={id} type={0}/>)
       break;
     case 2:
-      headerColor = "header-green";
+      headerColor = 'header-green';
+      childElem.unshift(<Task text='Implement Header Colors' id={id} type={0}/>)
       // childElem.push(<Task text="test text 3" id={0}/>)
       break;
     default:
@@ -31,8 +33,8 @@ function Column({name, id} : ColProps) {
   }
   console.log(childElem);
   return (
-    <div className="board-col">
-      <div className={"col-header " + headerColor}>
+    <div className='board-col'>
+      <div className={'col-header ' + headerColor}>
         {name}
       </div>
       {(childElem) ? childElem : <div></div>}
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -5,13 +5,31 @@ import '../style/Board.css';
 
 type ColProps = {
   text: string,
-  id: number
+  type: number,
+  id: number,
 }
 
-function Task({text} : ColProps) {
+function Task({text, type, id} : ColProps) {
   return (  
-    <div className="task-row">
-      {text}
+    <div className='task-row'>
+      <span className='left-angle'>
+      {(id > 0 && type > -1) ? <span>
+        &#x2039;
+      </span> : undefined}
+      </span>
+      <span className={type < 0 ? 'create-text' : 'task-text'} onClick={(e) => {
+        if (type < 0) {
+          var inputText = prompt();
+          alert(inputText);
+        }
+      }}>
+        {text}
+      </span>
+      <span className='right-angle'>
+      {(id < 2 && type > -1) ? <span>
+        &#8250;
+      </span> : undefined}
+      </span>
     </div>
   );
 }
