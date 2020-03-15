35cdc0458e4e9c4ad2554cacf99405facdfa6719
Justin Ang <justinang13@gmail.com>
2020-03-15 15:57:11 -0700

    working on TS Redux implementation

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
 import '../style/Board.css';
 
 import Column from './Column';
 
+interface System {
+  system: {
+  };
+};
+
+const typedUseSelector: TypedUseSelectorHook<System> = useSelector;
+
 function Board() {
+  const system = typedUseSelector(state => state.system);
+  console.log(system);
   return (  
     <div className="main-board">
       <div className="col-container">
